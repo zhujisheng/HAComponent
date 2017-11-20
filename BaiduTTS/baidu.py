7702b5d346cabd6301a32003ca7f8b97590403ff
zhujisheng <30714273+zhujisheng@users.noreply.github.com>
2017-11-20 19:03:32 +0800

    Add files via upload

--- BaiduTTS/baidu.py
+++ BaiduTTS/baidu.py
@@ -1,14 +1,13 @@
 """
-Baidu TTS　HAChina
+Support for the baidu speech service.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/tts.baidu/
 """
+
 import asyncio
 import logging
-import re
-
-import aiohttp
-import async_timeout
 import voluptuous as vol
-
 
 from homeassistant.components.tts import Provider, PLATFORM_SCHEMA, CONF_LANG
 import homeassistant.helpers.config_validation as cv
@@ -38,11 +37,14 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_APPID): cv.string,
     vol.Required(CONF_APIKEY): cv.string,
     vol.Required(CONF_SECRETKEY): cv.string,
-    vol.Optional(CONF_SPEED, default=5): vol.All(vol.Coerce(int), vol.Range(min=0,max=9)),
-    vol.Optional(CONF_PITCH, default=5): vol.All(vol.Coerce(int), vol.Range(min=0,max=9)),
-    vol.Optional(CONF_VOLUME, default=5): vol.All(vol.Coerce(int), vol.Range(min=0,max=15)),
-    vol.Optional(CONF_PERSON, default=0): vol.All(vol.Coerce(int), vol.Range(min=0,max=4)),
-    
+    vol.Optional(CONF_SPEED, default=5): vol.All(
+        vol.Coerce(int), vol.Range(min=0, max=9)),
+    vol.Optional(CONF_PITCH, default=5): vol.All(
+        vol.Coerce(int), vol.Range(min=0, max=9)),
+    vol.Optional(CONF_VOLUME, default=5): vol.All(
+        vol.Coerce(int), vol.Range(min=0, max=15)),
+    vol.Optional(CONF_PERSON, default=0): vol.All(
+        vol.Coerce(int), vol.Range(min=0, max=4)),
 })
 
 
@@ -59,17 +61,21 @@ class BaiduTTSProvider(Provider):
         """Init Baidu TTS service."""
         self.hass = hass
         self._lang = conf.get(CONF_LANG)
-        self._appid = conf.get(CONF_APPID)
-        self._apikey = conf.get(CONF_APIKEY)
-        self._secretkey = conf.get(CONF_SECRETKEY)
-
-        self._speed = conf.get(CONF_SPEED)
-        self._pitch = conf.get(CONF_PITCH)
-        self._volume = conf.get(CONF_VOLUME)
-        self._person = conf.get(CONF_PERSON)
-        
         self._codec = 'mp3'
         self.name = 'BaiduTTS'
+
+        self._app_data = {
+            'appid': conf.get(CONF_APPID),
+            'apikey': conf.get(CONF_APIKEY),
+            'secretkey': conf.get(CONF_SECRETKEY),
+            }
+
+        self._speech_conf_data = {
+            'spd': conf.get(CONF_SPEED),
+            'pit': conf.get(CONF_PITCH),
+            'vol': conf.get(CONF_VOLUME),
+            'per': conf.get(CONF_PERSON),
+            }
 
     @property
     def default_language(self):
@@ -85,23 +91,21 @@ class BaiduTTSProvider(Provider):
     def async_get_tts_audio(self, message, language, options=None):
         """Load TTS from BaiduTTS."""
         from aip import AipSpeech
-        aipSpeech = AipSpeech( self._appid, self._apikey, self._secretkey )
+        aip_speech = AipSpeech(
+            self._app_data['appid'],
+            self._app_data['apikey'],
+            self._app_data['secretkey']
+            )
 
-        result = aipSpeech.synthesis( message, language, 1,
-                                      {'spd':self._speed,
-                                       'pit':self._pitch,
-                                       'vol':self._volume,
-                                       'per':self._person
-                                       },                                      
-                                      )
+        result = aip_speech.synthesis(
+            message, language, 1, self._speech_conf_data)
 
         if isinstance(result, dict):
-            _LOGGER.error('Baidu TTS error-- err_no:%d; err_msg:%s; err_detail:%s',
-                          result['err_no'],
-                          result['err_msg'],
-                          result['err_detail']
-                         )
+            _LOGGER.error(
+                "Baidu TTS error-- err_no:%d; err_msg:%s; err_detail:%s",
+                result['err_no'],
+                result['err_msg'],
+                result['err_detail'])
             return (None, None)
 
         return (self._codec, result)
-
