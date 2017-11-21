9cd3577a6313d15a735f17c0a80bc707f0d9fc87
zhujisheng <30714273+zhujisheng@users.noreply.github.com>
2017-11-21 10:13:03 +0800

    Update baidu.py

--- BaiduTTS/baidu.py
+++ BaiduTTS/baidu.py
@@ -1,8 +1,6 @@
 """
 Support for the baidu speech service.
 
-For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/tts.baidu/
 """
 
 import asyncio
